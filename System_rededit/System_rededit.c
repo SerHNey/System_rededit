51949e8f33c1ea68d073fdac89d5f14d71fc830b
Нечто Страшное <naumov-1212@Mail.ru>
2022-12-09 18:52:56 +0300

    Добавьте файлы проекта.

---
+++ System_rededit/System_rededit.c
@@ -0,0 +1,27 @@
+﻿#include <Windows.h>
+#include <stdio.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow) {
+    HKEY hKey = NULL;
+    //if (RegOpenKey(HKEY_CURRENT_USER, NULL, &hKey) != ERROR_SUCCESS) // Открываем раздел в реестре 
+    //    return 1;
+    if (RegOpenKeyW(HKEY_CURRENT_USER, L"Time",&hKey) != ERROR_SUCCESS) // Открываем раздел в реестре 
+        return 1;
+
+    HKEY nametmp = NULL;
+    if (RegCreateKey(hKey, L"Time", &nametmp) == ERROR_SUCCESS) { // Создаём раздел в HKEY_CURRENT_USER
+        RegCloseKey(nametmp);
+        MessageBox(NULL, L"Успешно создан", L"Оповещение", MB_OK);
+    }
+
+    if (RegSetValue(hKey, nametmp, REG_SZ, L"Значение по умолчанию", 10 * sizeof(WCHAR)) == ERROR_SUCCESS) { // Создаём подраздел + задаём значение + указываем тип 
+        MessageBox(NULL, L"Значение присвоено", L"Оповезение", MB_OK);
+    }
+
+}
+
+int main()
+{
+
+}
+
