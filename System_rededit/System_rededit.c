b32c147ddb13b9ebb3c140caeb767039a6fef492
Нечто Страшное <naumov-1212@Mail.ru>
2022-12-09 20:18:18 +0300

    qa

--- System_rededit/System_rededit.c
+++ System_rededit/System_rededit.c
@@ -3,21 +3,35 @@
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow) {
     HKEY hKey = NULL;
-    //if (RegOpenKey(HKEY_CURRENT_USER, NULL, &hKey) != ERROR_SUCCESS) // Открываем раздел в реестре 
-    //    return 1;
-    if (RegOpenKeyW(HKEY_CURRENT_USER, L"Time",&hKey) != ERROR_SUCCESS) // Открываем раздел в реестре 
+    if (RegOpenKey(HKEY_CURRENT_USER, NULL, &hKey) != ERROR_SUCCESS) // Открываем раздел в реестре 
         return 1;
 
     HKEY nametmp = NULL;
     if (RegCreateKey(hKey, L"Time", &nametmp) == ERROR_SUCCESS) { // Создаём раздел в HKEY_CURRENT_USER
-        RegCloseKey(nametmp);
+        //RegCloseKey(nametmp); // Если закрыть, то нельзя создать по этому пути параметр в ново созданном разделе
         MessageBox(NULL, L"Успешно создан", L"Оповещение", MB_OK);
     }
 
-    if (RegSetValue(hKey, nametmp, REG_SZ, L"Значение по умолчанию", 10 * sizeof(WCHAR)) == ERROR_SUCCESS) { // Создаём подраздел + задаём значение + указываем тип 
-        MessageBox(NULL, L"Значение присвоено", L"Оповезение", MB_OK);
+    LPWSTR StrParam = L"Изменённый параметр1";
+    DWORD StrParamLen = wcslen(StrParam);
+
+    if (RegSetValue(hKey, L"Time", REG_SZ, StrParam, StrParamLen * sizeof(WCHAR)) == ERROR_SUCCESS) { // Создаём подраздел + задаём значение + указываем тип 
+        MessageBox(NULL, L"Значение присвоено", L"Оповещение", MB_OK);
     }
 
+    StrParam = L"Изменённый параметр2";
+
+    if (RegSetValueExW(nametmp, L"time", NULL, REG_SZ, StrParam, StrParamLen * sizeof(WCHAR)) == ERROR_SUCCESS) { // Создание ключа в разделе
+        MessageBox(NULL, L"Параметр успешно создан и ему присвоенно значение", L"Оповещение", MB_OK);
+    }
+
+    DWORD flag = 0;
+    LPWSTR readtext = malloc(512);
+    DWORD len = 500;
+
+    if (RegGetValue(nametmp, NULL, L"time",  RRF_RT_ANY, flag, readtext, &len) == ERROR_SUCCESS) {
+        MessageBox(NULL, readtext, L"Оповещение", MB_OK);
+    }
 }
 
 int main()
